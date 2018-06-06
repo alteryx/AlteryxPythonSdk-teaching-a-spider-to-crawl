c3823f0f638c2fca71c7d2d7c2e26df851d71f96
Chadwick <mchadwick@alteryx.com>
2018-06-05 23:57:08 -0600

    Clean up error handling

--- Scripts/ourfirstscraper/ourfirstscraper/spiders/redditbot.py
+++ Scripts/ourfirstscraper/ourfirstscraper/spiders/redditbot.py
@@ -9,40 +9,24 @@ class RedditbotSpider(scrapy.Spider):
     handle_httpstatus_list = range(0, 500)
 
     def parse(self, response):
-        print("parsing")
-        if response.status not in [200]:
-            item = {}
-            item['url'] = response.url
-            item['meta'] = response.meta
-            item['status'] = response.status
- 
-            yield item
- 
-        else:
+        if response.status == 200 and response.body:
             titles = response.css('.title.may-blank::text').extract()
             votes = response.css('.score.unvoted::text').extract()
             times = response.css('time::attr(title)').extract()
             comments = response.css('.comments::text').extract()
-       
-            data = zip(titles,votes,times,comments)
-            if len(titles) < 1:
-                item = {}
-                item['url'] = response.url
-                item['meta'] = response.meta
-                item['status'] = response.status
-    
-                yield item
-            
-            else:
-                #Give the extracted content row wise
-                for item in data:
-                    #create a dictionary to store the scraped info
-                    scraped_info = {
-                        'title' : item[0],
-                        'vote' : item[1],
-                        'created_at' : item[2],
-                        'comments' : item[3],
-                    }
-
-                    #yield or give the scraped info to scrapy
-                    yield scraped_info
+            #Give the extracted content row wise
+            for item in zip(titles,votes,times,comments):
+                #create a dictionary to store the scraped info
+                scraped_info = {
+                    'title' : item[0],
+                    'vote' : item[1],
+                    'created_at' : item[2],
+                    'comments' : item[3],
+                }
+                #yield or give the scraped info to scrapy
+                yield scraped_info
+        item = {}
+        item['url'] = response.url
+        item['meta'] = response.meta
+        item['status'] = response.status
+        yield item
